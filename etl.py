3f710b04adca827159dadc2e6fb7d77471cf1093
klongluang <klongluang10761@gmail.com>
2025-06-13 13:30:34 +0700

    v2

--- etl.py
+++ etl.py
@@ -1,11 +1,20 @@
 import pandas as pd
-import math
+from datetime import datetime
 
-# E
-df=pd.read_csv("sample_products.csv")
+dfInput = pd.read_csv('sample_products.csv')
+dfOutput = pd.read_csv('output/cleaned_products.csv')
 
-# T
-df["unit_price"] = df["unit_price"].apply(math.ceil)
+with open('output/test_result.txt', 'a') as f:
+    f.write(datetime.now().strftime("%d-%m-%Y %H:%M:%S") + '\n')
 
-# L
-df.to_csv("output/cleaned_products", index=False)
+    if dfOutput.dtypes['unit_price'] == 'int64':
+        f.write("Case 1: Pass\n")
+    else:
+        f.write("Case 1: Fail\n")
+
+    merged = pd.merge(dfInput, dfOutput, on='product_id', suffixes=('_input', '_output'))
+    diff = merged['unit_price_output'] - merged['unit_price_input']
+    if ((diff >= 0) & (diff <= 1)).all():
+        f.write("Case 2: Pass\n")
+    else:
+        f.write("Case 2: Fail\n")
